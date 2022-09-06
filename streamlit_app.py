578c63ac21e5dd68526f7cb47ef18d25468b65c6
marktimo44 <112555565+marktimo44@users.noreply.github.com>
2022-09-06 10:08:07 +0100

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -9,4 +9,5 @@ st.header('🍌🥝Build your own smoothies🍍🍑')
 
 import pandas as pd
 my_fruit_list = pd.read_csv("https://uni-lab-files.s3.us-west-2.amazonaws.com/dabw/fruit_macros.txt")
+st.dataframe(df, 200, 100)
 st.dataframe(my_fruit_list)
