3d01f55eda98fbe2faf8b22bd0c4d038474574f6
marktimo44 <112555565+marktimo44@users.noreply.github.com>
2022-09-06 09:56:18 +0100

    Update streamlit_app.py
    Added emoji's

--- streamlit_app.py
+++ streamlit_app.py
@@ -2,5 +2,5 @@ import streamlit as st
 
 st.title('My parents new healthy diner')
 st.header('Breakfast Menu')
-st.text('Full English')
-st.text('Bread Butty')
+st.text('🥓Full English')
+st.text('🍟Bread Butty')
