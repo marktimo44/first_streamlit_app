d5e3b6b3888027d218f6d36ca916b0800b806e00
marktimo44 <112555565+marktimo44@users.noreply.github.com>
2022-09-06 11:43:15 +0100

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -16,6 +16,6 @@ fruits_selected = st.multiselect("Pick some fruits:", list(my_fruit_list.index),
 fruits_to_show = my_fruit_list.loc[fruits_selected]
 
 # Display the table on the page.
-st.table(my_fruit_list)
+st.table(fruits_to_show)
 
 
