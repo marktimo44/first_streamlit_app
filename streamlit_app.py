dd68d726f664e47c083c743003b6d903cd611e37
marktimo44 <112555565+marktimo44@users.noreply.github.com>
2022-09-06 10:01:36 +0100

    Update streamlit_app.py
    Added list of fruits

--- streamlit_app.py
+++ streamlit_app.py
@@ -6,3 +6,7 @@ st.text('🥓Full English')
 st.text('🍟Bread Butty')
 
 st.header('🍌🥝Build your own smoothies🍍🍑')
+
+import pandas as pd
+my_fruit_list = pd.read_csv("https://uni-lab-files.s3.us-west-2.amazonaws.com/dabw/fruit_macros.txt")
+st.dataframe(my_fruit_list)
