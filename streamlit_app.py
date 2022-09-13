27ee5f0ecf04b23647beb47f98e93a0199dd0138
marktimo44 <112555565+marktimo44@users.noreply.github.com>
2022-09-13 15:34:31 +0100

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -19,7 +19,7 @@ fruits_to_show = my_fruit_list.loc[fruits_selected]
 st.table(fruits_to_show)
 
 import requests
-#fruityvice_response = requests.get("https://fruityvice.com/api/fruit/watermelon")
+fruityvice_response = requests.get("https://fruityvice.com/api/fruit/watermelon")
 #streamlit.text(fruityvice_response)
 
 
