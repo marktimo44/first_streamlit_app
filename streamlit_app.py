c8227d59cd2dd942400598bd5ecfb9e15e1dd36c
marktimo44 <112555565+marktimo44@users.noreply.github.com>
2022-09-13 15:42:03 +0100

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -21,7 +21,10 @@ st.table(fruits_to_show)
 st.header("Fruityvice Fruit Advice!")
 
 import requests
-fruityvice_response = requests.get("https://fruityvice.com/api/fruit/watermelon")
+fruit_choice = streamlit.text_input('What fruit would you like information about?','Kiwi')
+streamlit.write('The user entered ', fruit_choice)
+
+fruityvice_response = requests.get("https://fruityvice.com/api/fruit/" + fruit_choice)
 
 # Flatten with header
 fruityvice_normalized = pd.json_normalize(fruityvice_response.json())
